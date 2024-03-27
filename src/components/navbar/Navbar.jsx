91c1f3898ba89e29ac8f37785ef7c66209c63357
asim571 <160458924+asim571@users.noreply.github.com>
2024-03-27 16:47:48 +0545

    Card added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,31 +1,21 @@
-import React from 'react';
-import './Navbar.css';
+import React from 'react'
+import './Navbar.css'
 
 const Navbar = () => {
   return (
     <nav>
-      <div className="container">
-        <div className="logo">PMS</div>
-        <div id="navbar-menu" className="nav-links">
-          <ul>
-            {/* Add your navigation links here */}
-            <li>
-              <a href="#">Home</a>
-            </li>
-            <li>
-              <a href="#">About</a>
-            </li>
-            <li>
-              <a href="#">Services</a>
-            </li>
-            <li>
-              <a href="#">Contact</a>
-            </li>
-          </ul>
+        <div className="container">
+            <div className="logo">PMS</div>
+            <ul className="nav-links">
+                {/* <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact</a></li> */}
+            </ul>
+            <a href="#" class="add-product-btn">Add Product</a>
         </div>
-      </div>
     </nav>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
